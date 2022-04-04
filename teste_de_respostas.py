394f46d315fac6812dbccf84c044dbfd50d48548
Danilo0000000 <100388058+Danilo0000000@users.noreply.github.com>
2022-04-04 09:58:02 -0300

    Update teste_de_respostas.py

--- teste_de_respostas.py
+++ teste_de_respostas.py
@@ -1,64 +1,26 @@
 
-import random
-
-print("-----------------")
-print("Guessing Game")
-print("-----------------")
-attemps=0
-secret_number=0
-level=int(input("Which dificulty you want play?""\nType 1 for Easy (1/5 chance) \nType 2 for Medium (1/8 chance)\nType 3 for Hard (1/18 chance)\n"))
-if (level==1):
-    attemps=20
-    secret_number=random.randrange(1,101)
-    points=1000
-    print("The number is beetwen 1 and 100")
+question_1=((input("Did you call the victim?(type T for yes F to no):")))
+question_1=question_1.upper()
+test_1= 1 if question_1=="T" else 0
+question_2=((input("Were you at the crime scene?(type T for yes F to no):")))
+question_2=question_2.upper()
+test_2= 1 if question_2=="T" else 0
+question_3=((input("Do you live near the victim?(type T for yes F to no):")))
+question_3=question_3.upper()
+test_3= 1 if question_3=="T" else 0
+question_4=((input("Did it owe the victim?(type T for yes F to no):")))
+question_4=question_4.upper()
+test_4= 1 if question_4=="T" else 0
+question_5=((input("Have you ever worked with the victim?(type T for yes F to no):")))
+question_5=question_5.upper()
+test_5= 1 if question_5=="T" else 0
+tests= test_1+test_2+test_3+test_4+test_5
+if tests==2:
+    print("Suspect")
+elif tests ==3 or tests==4:
+    print("Accomplice")
+elif tests==5:
+    print("Killer")
 else:
-    if (level==2):
-        attemps=12 
-        secret_number=random.randrange(1,101)
-        print("The number is between 1 and 100")
-        points=1200
-    else:
-        if(level==3):
-            attemps=5
-            secret_number=random.randrange(1,91)
-            print("The number is between 1 and 90")
-            points=1800
-tried_number=0
-attemp_number=1
-while (attemps>=attemp_number)and tried_number!=secret_number :
-    print("Attemp {} of {}".format(attemp_number, attemps))
-    tried_number=int(input('Try one number: '))
-    if (level==1 or level==2) and (tried_number<0 or tried_number>100):
-        print("You need to type a number between 1 and 100")
-    else:  
-        if(level==3 and tried_number<0 or tried_number>90):
-            print("You need to type a number between 1 and 90")
-            continue
-    print("You typed number", tried_number)
-    correct=tried_number==secret_number
-    higher=tried_number>secret_number
-    smaller=tried_number<secret_number
-    if higher:
-        print("Smaller number.")
-    else:
-        if smaller:
-            print('Higher number.')
-        else:
-            if correct:
-                print('Correctly.')
-    attemp_number=attemp_number+1
-if level==1:
-    points=points-(attemp_number*10)
-else:
-    if level==2:
-        points=points-(attemp_number*18)
-    else:
-        if level==3:
-            points=points-(attemp_number*50)
-if higher or smaller:
-    print("Try on next, the number was",secret_number)
-else: 
-    print("Good job Genius")
-    print("You did {} points".format (points))
-
+    print("Innocent")
+    
